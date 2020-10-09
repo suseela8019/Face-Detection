1871e9a14b4d60afdae66f6d8cd56eb73b1a78b8
Suseela Pulivarthi <58815417+suseela8019@users.noreply.github.com>
2020-10-09 23:51:50 +0530

    Update different expressions prog.py

--- different expressions prog.py
+++ different expressions prog.py
@@ -5,7 +5,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Read the input image using imread
-img = cv2.imread('face expressions.jpg ')
+img = cv2.imread('Images\face expressions.jpg ')
 
 # Convert into grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
