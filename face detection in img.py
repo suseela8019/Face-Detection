a1d43091d23878f8acec08ccc5379e73b3cedbc8
Suseela Pulivarthi <58815417+suseela8019@users.noreply.github.com>
2020-10-09 23:52:40 +0530

    Update face detection in img.py

--- face detection in img.py
+++ face detection in img.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 # Read the input image
 #img = cv2.imread('samefacediffmask.jpg')
-img = cv2.imread('multi-faces.jpg ')
+img = cv2.imread('Images\multi-faces.jpg ')
 
 
 # Convert into grayscale
